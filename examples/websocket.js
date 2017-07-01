b809b4f1ee3b8e7fc9c0fbc54809663a050f330f
Connor Giles <cgiles6@uwo.ca>
2017-07-01 12:49:24 -0700

    Changed bitstamp orderbook

--- examples/websocket.js
+++ examples/websocket.js
@@ -1,15 +1,15 @@
 const xchange = require('../lib/xchange')
 
-var client = new xchange.Bitfinex.WebsocketClient();
+var client = new xchange.Bitstamp.WebsocketClient();
 
 var pairs = ['BTCUSD']
 var markets = [];
 
 client.on('open', () => {
-	markets = pairs.map(p => client.subscribeRaw(p));
+	markets = pairs.map(p => client.subscribe(p));
 });
 
 client.on('change', (market) => {
 	// Get market's full book
-	console.log(market.pair, ':', JSON.stringify(market.book.state(), null, ' '));
+	console.log(market.pair, ':', JSON.stringify(market.book.depth(2), null, ' '));
 });
