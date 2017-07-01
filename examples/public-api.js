710dbcec61eca0586dbe82b43aaa5ed66b38e705
Connor Giles <cgiles6@uwo.ca>
2017-07-01 12:32:01 -0700

    Changed Bitfinex to raw book

--- examples/public-api.js
+++ examples/public-api.js
@@ -1,6 +1,6 @@
 const xchange = require('../lib/xchange');
 
-var client = new xchange.Gdax.PublicClient();
+var client = new xchange.Bitfinex.PublicClient();
 
 client.trades('ETHBTC').then(console.log).catch(console.error);
 // client.orderbook('ETHBTC').then(m => console.log(m.bookOfDepth(5))).catch(console.error);
--- examples/websocket.js
+++ examples/websocket.js
@@ -2,19 +2,14 @@ const xchange = require('../lib/xchange')
 
 var client = new xchange.Bitfinex.WebsocketClient();
 
-var pairs = ['BTCUSD', 'LTCBTC', 'XRPBTC']
+var pairs = ['BTCUSD']
 var markets = [];
 
 client.on('open', () => {
-	markets = pairs.map(p => client.subscribe(p));
-	console.log(markets);
+	markets = pairs.map(p => client.subscribeRaw(p));
 });
 
 client.on('change', (market) => {
 	// Get market's full book
-	console.log(market.pair, ':', JSON.stringify(market.bookOfDepth(), null, ' '));
-	// Get market's book 5 prices deep
-	console.log(market.pair, ':', JSON.stringify(market.bookOfDepth(5), null, ' '));
-	// Get market's top of book
-	console.log(market.pair, ':', JSON.stringify(market.topOfBook(), null, ' '));
+	console.log(market.pair, ':', JSON.stringify(market.book.state(), null, ' '));
 });
