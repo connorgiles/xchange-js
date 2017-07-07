81d8d936cf1d4e59b7b267208cf0e5c0fe5eee89
Connor Giles <cgiles6@uwo.ca>
2017-07-06 23:43:24 -0400

    This is broken

--- examples/public-api.js
+++ examples/public-api.js
@@ -1,8 +1,8 @@
 const xchange = require('../lib/xchange');
 
-var client = new xchange.Bitfinex.PublicClient();
+var client = new xchange.Bitstamp.PublicClient();
 
-client.trades('ETHBTC').then(console.log).catch(console.error);
-// client.orderbook('ETHBTC').then(m => console.log(m.bookOfDepth(5))).catch(console.error);
+// client.trades('ETHBTC').then(console.log).catch(console.error);
+client.orderbook('LTCBTC').then(m => console.log(m.book.depth(4))).catch(console.error);
 
 // client.pairs().then(console.log).catch(console.error);
--- examples/websocket.js
+++ examples/websocket.js
@@ -1,19 +1,26 @@
 const xchange = require('../lib/xchange')
 
-var client = new xchange.Poloniex.WebsocketClient();
+var client = new xchange.Gdax.WebsocketClient();
 
-var pair = 'ETHBTC';
+var pair = 'BTCUSD';
 var market;
 
 client.on('open', () => {
 	market = client.subscribe(pair);
-	market.book.subscribe(1);
+	market.book.subscribe(5);
+
 	market.book.on('change', (depth, book) => {
-		console.log(depth);
 		console.log(book);
-	// Get market's full book
-	// console.log(market.pair, ':', JSON.stringify(market.book.depth(5), null, ' '));
-});
+	});
+
 
 });
 
+
+client.on('error', (data) => {
+	console.log(data)
+})
+
+client.on('close', (data) => {
+	console.log('Closed')
+})
